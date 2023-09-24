a007845309b79b2c74b4c71fbad0e86fd05c978d
Kris De Volder <kris.de.volder@gmail.com>
2023-09-24 15:33:06 +0000

    Description for Dlist in docs

--- searchspace/treequence.mli
+++ searchspace/treequence.mli
@@ -1,3 +1,10 @@
-include Dlist_itf.S
+(**
+ A [Treequence] is an implementation `Dlist` which keeps its elements in a tree. 
+ Every operation that adds elements to Treequence is O(1). Operations that retrieve
+ or remove elements from it will restructure the tree to expose either the front
+ or back of the list for fast access. This allows for efficient accesses on both ends
+ but there is a cost to be paid when switching between accessing from the 
+ front/back or the back.*)
+ include Dlist_itf.S
 module Persistable :
   functor (A : Persist.Persistable) -> Persist.Persistable with type t = A.t t
