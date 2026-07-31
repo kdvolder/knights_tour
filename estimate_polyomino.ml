6450785717d62737a110070243aa2af19b018352
kdvolder-agent <kris.de.volder+ai@gmail.com>
2026-07-31 00:48:50 -0700

    Cleanup some logging

--- estimate_polyomino.ml
+++ estimate_polyomino.ml
@@ -19,14 +19,6 @@ let init_runtime_events () =
     flush stderr
   end
 
-let log_heap_usage batch_num =
-  if !re_available then begin
-    Searchspace.poll_runtime_events ();
-    let mb = Searchspace.heap_usage_mb () in
-    Printf.eprintf "[RuntimeEvents] Batch %d: heap = %.1f MB\n" batch_num mb;
-    flush stderr
-  end
-
 let solutions_file_extension () =
   let today = Unix.localtime (Unix.time ()) in
   let day = today.tm_mday in
@@ -57,11 +49,12 @@ let () =
     In_channel.with_open_text puzzle_file Puzzle.load
   in
   let searchspace = Puzzle.solve puzzle in
-  let selector = Stochastic_estimator.memory_aware_selector ~threshold:0.3 () in
+  let (selector, _) = Stochastic_estimator.gradual_braking_selector ~threshold_mb:8. () in
+  (*let selector = Stochastic_estimator.memory_aware_selector ~threshold:0.3 () in*)
   (* let selector = Stochastic_estimator.probabilistic_undersampled_selector in *)
   let estimator = Stochastic_estimator.create ~on_solution ~selector searchspace in
   
-  Printf.printf "%-5s | %-7s | %-12s | %-12s | %-8s | %-6s | %-12s | %-10s | %-10s | %-8s | %-10s | %s\n%!" "Batch" "Samples" "Nodes Est" "Fails Est" "Sols Est" "Found" "Materialized" "%Complete" "Pruned" "%Free" "Elapsed" "ETA";
+  Printf.printf "%-5s | %-7s | %-12s | %-12s | %-8s | %-6s | %-12s | %-10s | %-10s | %-8s | %-10s | %s\n%!" "Batch" "Samples" "Nodes Est" "Fails Est" "Sols Est" "Found" "Materialized" "%Complete" "Pruned" "Mem.Mb" "Elapsed" "ETA";
   Printf.printf "----- | ------- | ------------ | ------------ | -------- | ------ | ------------ | ---------- | ---------- | -------- | ---------- | ------------------\n%!";
   
   let batch_count = ref 0 in
@@ -70,7 +63,7 @@ let () =
     incr batch_count;
     total_samples := !total_samples + batch_size;
     let est = Stochastic_estimator.estimates estimator in
-    let free_pct = Searchspace.memfree () *. 100. in
+    let free_pct = Searchspace.heap_usage_mb () in
     Printf.printf "%-5d | %-7d | %12s | %12s | %-8s | %-6d | %-12d | %10.2e%% | %-10d | %-8.1f | %-10s | %s\n%!"
       !batch_count !total_samples
       (format_number est.nodes) (format_number est.fails) (format_number est.solutions)
@@ -80,10 +73,7 @@ let () =
       p.pruned_nodes
       free_pct
       (Stochastic_estimator.format_time p.elapsed_seconds)
-      (Stochastic_estimator.format_time p.estimated_remaining_seconds);
-    
-    (* Log heap usage *)
-    log_heap_usage !batch_count
+      (Stochastic_estimator.format_time p.estimated_remaining_seconds)
   ) estimator;
   
   (* Final report *)
